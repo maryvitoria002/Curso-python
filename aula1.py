e12152f72f59382b167e30531fc5f6fc9a86a84f
Mary <146475589+maryvitoria002@users.noreply.github.com>
2025-01-20 21:24:05 -0300

    aula #4 do curso do guanabara

---
+++ aula1.py
@@ -0,0 +1,6 @@
+nome = input("Qual o seu nome?")
+idade = input('Quantos anos ce tem?')
+peso = input ('e qual teu peso irmão')
+
+print(nome, idade, peso)
+
---
+++ desafio2.py
@@ -0,0 +1,5 @@
+dia = input('Que dia tu nasceu? ')
+mes = input('De que mes? ')
+ano = input('E em que ano que foi? ')
+
+print('Voce nasceu no dia', dia, 'de', mes, 'de', ano, '. correto?')
