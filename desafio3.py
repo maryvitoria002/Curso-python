e4c3282010bd4bdb38d8fe4b3502a2d6eac1c9e5
Mary <146475589+maryvitoria002@users.noreply.github.com>
2025-01-22 09:38:08 -0300

    exercicio 1 e 2

---
+++ desafio3.py
@@ -0,0 +1,4 @@
+numero1 = int(input('Primero número'))
+numero2 = int(input('Segundo número'))
+
+print(numero1 + numero2)
---
+++ ex002.py
@@ -0,0 +1,2 @@
+nome = input('Qual seu nome? ')
+print("Seja bem-vindo(a), {}." .format(nome))
