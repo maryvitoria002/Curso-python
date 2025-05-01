2747ee22ed50a0dc272a03d2f9212809860b3c97
Mary <146475589+maryvitoria002@users.noreply.github.com>
2025-05-01 17:56:32 -0300

    aula de estrutura de repetição

---
+++ PythonTeste/aula13.py
@@ -0,0 +1,20 @@
+# for i in range (10, -2, -2):  
+#     print (i)
+# print("FIM")
+
+
+# n = int(input("Início: "))
+# f = int(input("Fim: "))
+# p = int(input("Passo: "))
+
+# for i in range(n, f+1, p):
+#     print (i)
+# print("Fim")
+
+s = 0
+for i in range (0, 3):
+    n = int(input("Digite um valor: "))
+    s += n
+
+print(f"A soma disso tudo aí é {s}")
+
